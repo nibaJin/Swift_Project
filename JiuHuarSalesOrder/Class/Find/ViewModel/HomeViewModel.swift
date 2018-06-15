f130cab60ccc50932ad1c505ddae0a6ab5b547e3
nibaJin <jinfu@jindeMacBook-Pro.local>
2018-06-15 11:22:39 +0800

    测试 commit

---
+++ JiuHuarSalesOrder/Class/Find/ViewModel/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  JiuHuarSalesOrder
+//
+//  Created by jin fu on 2018/6/15.
+//  Copyright © 2018年 jin fu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewModel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
