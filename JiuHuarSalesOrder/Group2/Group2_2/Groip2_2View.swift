0438cacedc6ce8efc6bb5768c818ed0ede2e8fcd
nibaJin <jinfu@jindeMacBook-Pro.local>
2018-06-15 10:46:10 +0800

    新增文件

---
+++ JiuHuarSalesOrder/Group2/Group2_2/Groip2_2View.swift
@@ -0,0 +1,21 @@
+//
+//  Groip2_2View.swift
+//  JiuHuarSalesOrder
+//
+//  Created by jin fu on 2018/6/15.
+//  Copyright © 2018年 jin fu. All rights reserved.
+//
+
+import UIKit
+
+class Groip2_2View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
