69d2c906c934c929ec121af88938f21571d58a96
nibaJin <jinfu@jindeMacBook-Pro.local>
2018-06-15 10:39:07 +0800

    文件提交 测试

---
+++ JiuHuarSalesOrder/Group3View.swift
@@ -0,0 +1,21 @@
+//
+//  Group3View.swift
+//  JiuHuarSalesOrder
+//
+//  Created by jin fu on 2018/6/14.
+//  Copyright © 2018年 jin fu. All rights reserved.
+//
+
+import UIKit
+
+class Group3View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
